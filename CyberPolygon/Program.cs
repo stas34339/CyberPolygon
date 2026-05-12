ba123a81840bdf1e266950dbb23fb8a9456a24ef
stas34339 <stas34339@mail.ru>
2026-05-12 15:18:54 +0300

    Добавлена страница авторизации(без функционала)

--- CyberPolygon/Program.cs
+++ CyberPolygon/Program.cs
@@ -1,48 +1,62 @@
 using CyberPolygon.Components;
 using CyberPolygon.Components.Account;
 using CyberPolygon.Data;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Radzen;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// 1. Базовые сервисы Blazor .NET 8
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+// Обязательно добавляем поддержку Razor Pages, так как Identity (даже Core) на ней базируется
+builder.Services.AddRazorPages();
 
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddScoped<IdentityRedirectManager>();
 builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
 
-builder.Services.AddAuthentication(options =>
-    {
-        options.DefaultScheme = IdentityConstants.ApplicationScheme;
-        options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
-    })
-    .AddIdentityCookies();
-
-
+// 2. Настройка БД
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+builder.Services.AddRadzenComponents();
+
+// 3. Настройка Identity
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = IdentityConstants.ApplicationScheme;
+    options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+})
+    .AddIdentityCookies();
 
 builder.Services.AddIdentityCore<ApplicationUser>(options =>
-    {
-        options.SignIn.RequireConfirmedAccount = true;
-        options.Stores.SchemaVersion = IdentitySchemaVersions.Version3;
-    })
+{
+    options.SignIn.RequireConfirmedAccount = false; // Поставил false для удобства разработки
+})
+    .AddRoles<IdentityRole>() // Добавь это, если планируешь использовать роли Admin/User
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddSignInManager()
     .AddDefaultTokenProviders();
 
 builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
 
+// 4. Настройка Cookie (Теперь сервисы RazorPages доступны, и ошибки не будет)
+builder.Services.ConfigureApplicationCookie(options => {
+    options.LoginPath = "/login";
+    options.AccessDeniedPath = "/access-denied";
+    options.Cookie.Name = "CyberPolygon.Auth";
+});
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// 5. Middleware Pipeline
 if (app.Environment.IsDevelopment())
 {
     app.UseMigrationsEndPoint();
@@ -50,19 +64,25 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+
 app.UseHttpsRedirection();
 
+// Важно: Static Assets и Antiforgery должны быть до эндпоинтов
+app.MapStaticAssets();
 app.UseAntiforgery();
 
-app.MapStaticAssets();
+app.UseAuthentication();
+app.UseAuthorization();
+
+// Порядок эндпоинтов
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
-// Add additional endpoints required by the Identity /Account Razor components.
 app.MapAdditionalIdentityEndpoints();
+
+// Для работы Blazor Server механизмов
+app.MapBlazorHub();
 
 app.Run();
